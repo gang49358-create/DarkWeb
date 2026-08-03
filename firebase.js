f81096f1b6b873fa1725d1689c88196c233d3960
gang49358-create <gang49358@gmail.com>
2026-08-03 17:11:54 +1000

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -11,7 +11,7 @@ getAuth
 "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 import { 
-getFirestore 
+getFirestore
 } from 
 "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
